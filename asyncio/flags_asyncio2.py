4ccde5e9367c7cfff34cb46327a439ae10c560ea
seul lee <blacksangi14@naver.com>
2018-10-19 10:37:14 +0900

    flags_asyncio 코드 추가

--- asyncio/flags_asyncio2.py
+++ asyncio/flags_asyncio2.py
@@ -16,8 +16,8 @@ DEFAULT_CONCUR_REQ = 5
 MAX_CONCUR_REQ = 1000
 
 class FetchError(Exception):
-    def __init__(self, contry_code):
-        self.contry_code = contry_code
+    def __init__(self, country_code):
+        self.country_code = country_code
 
 
 @asyncio.coroutine
@@ -57,4 +57,47 @@ def download_one(cc, base_url, semaphore, verbose):
     return Result(status, cc)
 
 
+@asyncio.coroutine
+def download_coro(cc_list, base_url, verbose, concur_req):
+    counter = collections.Counter()
+    semaphore = asyncio.Semaphore(concur_req)
+
+    to_do = [download_one(cc, base_url, semaphore, verbose) for cc in sorted(cc_list)]
+
+    to_do_iter = asyncio.as_complete(to_do)
+    if not verbose:
+        to_do_iter = tqdm.tqdm(to_do_iter, total=len(cc_list))
+
+    for future in to_do_iter:
+        try:
+            res = yield from future
+        except FetchError as exc:
+            country_code = exc.country_code
+            try:
+                error_msg = exc.__cause__.args[0]
+            except IndexError:
+                error_msg = exc.__cause__.__class__.__name__
+            if verbose and error_msg:
+                msg = '*** Error for {}: {}'
+                print(msg.format(country_code, error_msg))
+            status = HTTPStatus.error
+        else:
+            status = res.status
+
+        counter[status] += 1
+
+    return counter
+
+
+def download_many(cc_list, base_url, verbose, concur_req):
+    loop = asyncio.get_event_loop()
+    coro = downloader_coro(cc_list, base_url, verbose, concur_req)
+    counts = loop.run_until_complete(coro)
+    loop.close()
+
+    return counts
+
+
+if __name__ == '__main__':
+    main(download_many, DEFAULT_CONCUR_REQ, MAX_CONCUR_REQ)
 
