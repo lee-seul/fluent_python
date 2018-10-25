03db82762527e4f05846d2a481fede747793861d
seul lee <blacksangi14@naver.com>
2018-10-25 10:36:51 +0900

    add bulk

---
+++ metaclass/bulk_food.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+
+import model
+
+
+class LineItem(model.Entity):
+    description = model.NonBlank()
+    weight = model.Quantity()
+    price = model.Quantity()
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self):
+        return self.weight * self.price
+
---
+++ metaclass/model.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+
+class EntityMeta(type):
+
+    def __init__(cls, name, bases, attr_dict):
+        super().__init__(name, bases, attr_dict)
+        for key, attr in attr_dict.items():
+            if isinstance(attr, Validated):
+                type_name = type(attr).__name__
+                attr.storage_name = '_{}#{}'.format(type_name, key)
+
+
+class Entity(metaclass=EntityMeta):
+    """"""
+
+
